8f4d2488baf9b84218203945fd8f48734c304f14
Gordon T Cumming <dynaphoneimages@hotmail.com>
2017-12-18 13:15:53 +0000

    Youtube video files which are listed in the default view but then you can select a video and play it in the video-details page

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AppComponent } from './app.component';
+import { YouTubeService } from './you-tube.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { YouTubeComponent } from './you-tube/you-tube.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
+
+
+import { routing } from './router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    YouTubeComponent,
+    VideoDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    BrowserTransferStateModule,
+    MatFormFieldModule,
+    MatInputModule,
+  //  RouterModule,
+    routing
+  ],
+  exports:[
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [YouTubeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
